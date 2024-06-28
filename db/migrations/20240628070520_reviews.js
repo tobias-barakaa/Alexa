681ad62db63c977febe637367241ab57739e2dbd
tobias-barakaa <ngairato@gmail.com>
2024-06-28 10:07:47 +0300

    adding .env files

---
+++ db/migrations/20240628070520_reviews.js
@@ -0,0 +1,13 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('reviews', function(table) {
+      table.string('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
+      table.string('user_id').notNullable().references('id').inTable('users').onDelete('CASCADE');
+      table.text('comment');
+      table.timestamps(true, true);
+    });
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.dropTable('reviews');
+  };
+  
