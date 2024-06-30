947d4b3ceff07cd9d9ad78e8f1665328260b08df
tobias-barakaa <ngairato@gmail.com>
2024-06-30 17:46:11 +0300

    additional features

--- routes/admin/adminWritersRoute.js
+++ routes/admin/adminWritersRoute.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const { protectAdmin, verifyAdmin } = require('../../middlewares/admin/adminMiddleware.js');
-const { getWriters } = require('../../controllers/admin/writersAdminController.js');
+const { getWriters, assignArticle } = require('../../controllers/admin/writersAdminController.js');
 
 const router = express.Router();
 
 router.get("/", protectAdmin, verifyAdmin, getWriters);
+router.post("/", protectAdmin, verifyAdmin, assignArticle);
+
 
 
 module.exports = router;
