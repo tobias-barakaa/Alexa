5f24abd5d56f9d5b45abba31ee5f54c50ffc753a
tobias-barakaa <ngairato@gmail.com>
2024-07-01 09:44:54 +0300

    generate token function

--- utils/client/generateToken.js
+++ utils/client/generateToken.js
@@ -10,6 +10,8 @@ const generateToken = (res, id) => {
         maxAge: 30 * 24 * 60 * 60 * 1000
       })
 
+      console.log('Token generated and set in cookie:', token);
+
 }
 
 module.exports = generateToken;
