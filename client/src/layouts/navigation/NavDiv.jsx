a36d8a7b7018ba8b8ee5c0cf5da09c2c8dd875af
tobias-barakaa <ngairato@gmail.com>
2024-07-05 11:36:02 +0300

    cannot figure what style I need

--- client/src/layouts/navigation/NavDiv.jsx
+++ client/src/layouts/navigation/NavDiv.jsx
@@ -3,50 +3,36 @@ import "./NavDiv.css";
 
 
 const NavDiv = () => {
+
+  
   return (
-    <nav className="nav">
-    <div className="nav-logo">
-      <img src="logo.png" alt="Logo" />
-    </div>
-    <div className="nav-items">
-      <a href="#" className="nav-link">Home</a>
-      <a href="#" className="nav-link">Profile</a>
-      <a href="#" className="nav-link">Messages</a>
-      <div className="nav-item dropdown">
-        <a href="#" className="nav-link dropdown-toggle">
-          Our Services
-          <i className="fas fa-chevron-down"></i>
-        </a>
-        <div className="dropdown-menu">
-          <div className="dropdown-row">
-            <div className="dropdown-col">
-              <h3>Our Services</h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                euismod, nisl eget aliquam tincidunt, nisl nisl aliquam nisl,
-                eget aliquam nisl nisl eget nisl.
-              </p>
-            </div>
-            <div className="dropdown-col">
-              <a href="#" className="dropdown-link">Poetry Writing</a>
-              <a href="#" className="dropdown-link">Social Posts</a>
-              <a href="#" className="dropdown-link">Songs</a>
-              <a href="#" className="dropdown-link">Article Writing</a>
-              <a href="#" className="dropdown-link">Blog Posts</a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <a href="#" className="nav-link">Analytics</a>
-      <a href="#" className="nav-link">Settings</a>
-      <a href="#" className="nav-link">Logout</a>
-    </div>
-    <div className="nav-actions">
-      <a href="#" className="nav-link">Sign In</a>
-      <button className="nav-button">Get Started</button>
-    </div>
-    <hr className="nav-hr" />
-  </nav>
+  <>
+   <nav className="nav">
+  <div className="nav-logo">
+    Enwriters
+  </div>
+  <div className="nav-items">
+    <a href="#" className="nav-link">Home</a>
+    <a href="#" className="nav-link">About</a>
+    <a href="#" className="nav-link">Services</a>
+    <a href="#" className="nav-link dropdown-toggle">
+      Our Services
+      <i className="fas fa-chevron-down"></i>
+    </a>
+    <a href="#" className="nav-link">Contact</a>
+    <a href="#" className="nav-link">Blog</a>
+    <a href="#" className="nav-link">Portfolio</a>
+  </div>
+  <div className="nav-actions">
+    <a href="#" className="nav-link">Sign In</a>
+    <button className="nav-button">Get Started</button>
+  </div>
+  <hr className="nav-hr" />
+</nav>
+
+
+  </>
+  
   );
 };
 
