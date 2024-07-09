33419b88309ef787c99b0f6c189300c690fcd50d
tobias-barakaa <ngairato@gmail.com>
2024-07-09 11:34:01 +0300

    debugging...

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,7 +1,7 @@
 import process from 'process';
 
 export const BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3001' : 'https://api.example.com';
-export const USERS_URL = 'users';
+export const USERS_URL = '/api/users';
 export const ARTICLES = 'api/articles';
 export const COMMENTS = 'api/comments';
 export const PAYPAL_URL= '/api/articles'
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000/api/',
-        changeOrigin: true,
+        target: 'http://localhost:5000',
         secure: false,
-      },
-    },
+      }
+    }
   },
-})
+  plugins: [react()],
+
+});
