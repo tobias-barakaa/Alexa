ee73bb790140713bd84b7ae106af2385b8db67a3
tobias-barakaa <ngairato@gmail.com>
2024-07-15 10:00:36 +0300

    add more

--- client/src/slices/apiSlice.jsx
+++ client/src/slices/apiSlice.jsx
@@ -1,15 +1,3 @@
-// import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-// import { BASE_URL } from '../constants';
-
-// // const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL });
-// const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL + '/api' });
-
-// export const apiSlice = createApi({
-//     baseQuery,
-//     tagTypes: ['Articles', 'writers', 'users'],
-//     endpoints: () => ({})
-// })
-
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { BASE_URL } from '../constants';
 
