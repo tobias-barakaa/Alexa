219596ad36cc953422520a49c879207b6e053f70
tobias-barakaa <ngairato@gmail.com>
2024-07-17 19:46:38 +0300

    debugging

--- middlewares/client/authMiddleware.js
+++ middlewares/client/authMiddleware.js
@@ -24,29 +24,29 @@ const knex = require("../../db/db.js");
 // };
 
 const protect = async (req, res, next) => {
-    let token;
-    token = req.cookies.jwt;
-    
-    console.log('Cookies:', req.cookies); // Log cookies to check if the token is present
-  
-    if (token) {
-      try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = await knex('users')
-          .select('id', 'username', 'email', 'profile_pic', 'created_at', 'updated_at')
-          .where({ id: decoded.id })
-          .first();
-        
-        next();
-      } catch (error) {
-        console.error('Token verification failed:', error);
-        res.status(401).json({ message: 'Not authorized, token failed' });
-      }
-    } else {
-      console.error('No token found');
-      res.status(401).json({ message: 'Not authorized, no token' });
+  let token = req.cookies.jwt;
+  console.log('Cookies:', req.cookies); // Log cookies to check if the token is present
+
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await knex('users')
+        .select('id', 'username', 'email', 'profile_pic', 'created_at', 'updated_at')
+        .where({ id: decoded.id })
+        .first();
+      
+      console.log('User found:', req.user); // Log user details
+      next();
+    } catch (error) {
+      console.error('Token verification failed:', error);
+      res.status(401).json({ message: 'Not authorized, token failed' });
     }
-  };
+  } else {
+    console.error('No token found');
+    res.status(401).json({ message: 'Not authorized, no token' });
+  }
+};
+
 
   module.exports = {protect}
   
