2bd4ed7e7d739997251dd40aa40f5feb5be274ac
tobias-barakaa <ngairato@gmail.com>
2024-07-18 17:04:50 +0300

    userInfo

--- client/src/slices/articlesApiSlice.jsx
+++ client/src/slices/articlesApiSlice.jsx
@@ -2,6 +2,26 @@ import { ARTICLES } from "../constants";
 import { apiSlice } from './apiSlice';
 
 
+// export const articlesApiSlice = apiSlice.injectEndpoints({
+//   endpoints: (builder) => ({
+//     getArticles: builder.query({
+//       query: () => ({
+//         url: ARTICLES,
+//       }),
+//       keepUnusedDataFor: 5,
+//       credentials: 'include'
+//     }),
+//     orderArticles: builder.mutation({
+//       query: (data) => ({
+//         url: ARTICLES,
+//         method: 'POST',
+//         body: data,
+//         credentials: 'include',
+//       }),
+//     }),
+//   }),
+// });
+
 export const articlesApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getArticles: builder.query({
@@ -9,7 +29,7 @@ export const articlesApiSlice = apiSlice.injectEndpoints({
         url: ARTICLES,
       }),
       keepUnusedDataFor: 5,
-      credentials: 'include'
+      credentials: 'include',
     }),
     orderArticles: builder.mutation({
       query: (data) => ({
