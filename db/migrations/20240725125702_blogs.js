988bfaf6b654186d5ff310852f0a2e74cef46972
tobias-barakaa <ngairato@gmail.com>
2024-07-25 16:03:24 +0300

    creating blog table

---
+++ db/migrations/20240725125702_blogs.js
@@ -0,0 +1,26 @@
+ /**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('blogs', (table) => {
+        table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
+      table.string('title').notNullable();
+      table.integer('category_id').unsigned().notNullable().references('id').inTable('blogcategories');
+      table.text('tags_keywords').nullable();
+      table.text('excerpt').nullable();
+      table.integer('number_of_words_id').unsigned().notNullable().references('id').inTable('numberofwords');
+      table.integer('timeframe_id').unsigned().notNullable().references('id').inTable('timeframe');
+      table.string('user_id').notNullable().references('id').inTable('users'); 
+      table.timestamps(true, true); 
+    });
+  };
+  
+  /**
+   * @param { import("knex").Knex } knex
+   * @returns { Promise<void> }
+   */
+  exports.down = function(knex) {
+    return knex.schema.dropTable('blogs');
+  };
+  
