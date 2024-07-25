6f3f8213363cf14a042af292b790c72f8de50c4f
tobias-barakaa <ngairato@gmail.com>
2024-07-25 16:11:56 +0300

    succesfully created one table and three seeds

--- db/migrations/20240725125702_blogs.js
+++ db/migrations/20240725125702_blogs.js
@@ -4,14 +4,16 @@
  */
 exports.up = function(knex) {
     return knex.schema.createTable('blogs', (table) => {
-        table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
+      table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
       table.string('title').notNullable();
       table.integer('category_id').unsigned().notNullable().references('id').inTable('blogcategories');
-      table.text('tags_keywords').nullable();
-      table.text('excerpt').nullable();
+      table.text('tags').defaultTo('').nullable();
+      table.text('excerpt').defaultTo('').nullable();
       table.integer('number_of_words_id').unsigned().notNullable().references('id').inTable('numberofwords');
       table.integer('timeframe_id').unsigned().notNullable().references('id').inTable('timeframe');
       table.string('user_id').notNullable().references('id').inTable('users'); 
+      table.enum('status', ['draft', 'published', 'archived', 'pending review', 'rejected', 'scheduled']).defaultTo('draft');
+      table.timestamp('published_at');
       table.timestamps(true, true); 
     });
   };
