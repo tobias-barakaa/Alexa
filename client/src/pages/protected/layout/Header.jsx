6a50174a7881fd6dc481010181da9a26b1064df0
tobias-barakaa <ngairato@gmail.com>
2024-07-29 15:42:25 +0300

    changing css

--- client/src/pages/protected/layout/Header.jsx
+++ client/src/pages/protected/layout/Header.jsx
@@ -4,11 +4,18 @@ const Header = () => {
   return (
     <div>
      
-        <div className="top-navbar">
-          <div className="greey">
-            Home | About Website | FAQ | Contact us <span className="yel">mashida</span>
-          </div>
-        </div>
+     <div className="top-navbar">
+  <ul className="nav-list">
+    <li className="home">Home</li>
+    <li className="about">About Website</li>
+    <li className="faq">FAQ</li>
+    <li className="contact">Contact us</li>
+
+    <li className="menu"><span>⚙️</span>Settings</li>
+    <li className="what"><span>💳 </span>Wallet</li>
+
+  </ul>
+</div>
         
     </div>
   );
--- client/src/pages/protected/layout/Sidenav.jsx
+++ client/src/pages/protected/layout/Sidenav.jsx
@@ -6,24 +6,23 @@ const Sidebar = () => {
   return (
     <div className="sidena">
       <div className="sidebar-content-item">
-        <span>📦 Services</span>
+        <span className='services'>Services</span>
         <ul className="sidebar-dropdown">
-          <li><span><Link to="blogorder">📝 Blog Writing</Link></span></li>
-          <li><span><Link to="articlecreation">✍️ Article Creation</Link></span></li>
-          <li><span><Link to="resumecvwriting">📄 Resume/CV Writing</Link></span></li>
-          <li><span><Link to="emailcopywriting">📧 Email Copywriting</Link></span></li>
+          <li><span><Link to="blogorder"><span className='blog-writing'>📝</span> Blog Writing</Link></span></li>
+          <li><span><Link to="articlecreation"><span className='blog-writing'>✍️</span> Article Creation</Link></span></li>
+          <li><span><Link to="resumecvwriting"><span className='blog-writing'>📄</span> Resume/CV Writing</Link></span></li>
+          <li><span><Link to="emailcopywriting"><span className='blog-writing'>📧 </span>Email Copywriting</Link></span></li>
         </ul>
       </div>
       <div className="sidebar-content-item">
-        <span>📊 Manage Orders</span>
+        <span className='services'>Orders</span>
         <ul className="sidebar-dropdown">
           <li><span><Link to="editorders">🖋️ Edit Orders</Link></span></li>
           <li><span><Link to="ordershistory">🕰️ Orders History</Link></span></li>
           <li><span><Link to="poetryandsong">🏁 Completed Orders</Link></span></li>
         </ul>
       </div>
-      <div className="sidebar-item">⚙️ Settings</div>
-      <div className="sidebar-item">💳 Wallet</div>
+      
       <div className="sidebar-item">👤 Profile</div>
       <div className="sidebar-item">🚪 Logout</div>
     </div>
