fa13af9c0306bef6572fd12d6725e8f608da7a08
tobias-barakaa <ngairato@gmail.com>
2024-08-06 12:17:26 +0300

    trying to find a design to display blog and user at the same time update in real-time dropdown

--- controllers/admin/blogController.js
+++ controllers/admin/blogController.js
@@ -23,6 +23,38 @@ const getAllBlogs = async (req, res) => {
   }
 };
 
+
+const getBlogById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const blog = await knex('blogs')
+      .join('users', 'blogs.user_id', '=', 'users.id')
+      .select(
+        'blogs.id',
+        'blogs.title',
+        'blogs.tags',
+        'blogs.excerpt',
+        'blogs.status',
+        'blogs.published_at',
+        'blogs.created_at',
+        'blogs.updated_at',
+        'blogs.user_id'
+      )
+      .where('blogs.id', id)
+      .first();
+
+    if (!blog) {
+      return res.status(404).json({ error: 'Blog not found.' });
+    }
+
+    res.status(200).json({ blog });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Failed to fetch the blog.' });
+  }
+};
+
 module.exports = {
-  getAllBlogs
+  getAllBlogs,
+  getBlogById,
 };
--- routes/admin/blogRoute.js
+++ routes/admin/blogRoute.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const { protect } = require('../../middlewares/client/authMiddleware.js');
 const { protectAdmin, verifyAdmin } = require('../../middlewares/admin/adminMiddleware.js');
-const { getAllBlogs } = require('../../controllers/admin/blogController.js');
+const { getAllBlogs, getBlogById } = require('../../controllers/admin/blogController.js');
 
 const router = express.Router();
 
 router.get('/admin/blogs', protect, verifyAdmin, getAllBlogs);
+router.get('/admin/blogs/:id', protect, verifyAdmin, getBlogById);
+
+
+
 
 module.exports = router;
