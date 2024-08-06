358da4fb902458dc6e8360b62310d59963d19741
tobias-barakaa <ngairato@gmail.com>
2024-08-06 12:03:01 +0300

    receiving blog_id with user involved in the server

--- controllers/admin/blogController.js
+++ controllers/admin/blogController.js
@@ -13,11 +13,7 @@ const getAllBlogs = async (req, res) => {
         'blogs.published_at',
         'blogs.created_at',
         'blogs.updated_at',
-        'users.id as user_id',
-        // 'users.first_name as user_first_name',
-        // 'users.last_name as user_last_name',
-        // 'users.username as user_name',
-        // 'users.email as user_email'
+        
       );
 
     res.status(200).json({ blogs });
