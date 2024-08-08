c5ec5a38428c7843e58e62478f7c46494fb197e4
tobias-barakaa <ngairato@gmail.com>
2024-08-08 22:43:35 +0300

    install fortawesome

--- client/src/client/src/components/Header/Header.jsx
+++ client/src/client/src/components/Header/Header.jsx
@@ -1,9 +1,42 @@
+// src/components/Header.js
+import '../../styles/Header/Header.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
+
 const Header = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <header className="header">
+      <div className="logo">Logo</div>
+      <div className="spacer"></div>
+      <div className="nav-items">
+        <div className="nav-item">
+          <i className="fas fa-book beginner-icon"></i>
+          <span>Beginner Guide</span>
+        </div>
+        <div className="nav-item">
+          <i className="fas fa-glasses glossary-icon"></i>
+          <span>Glossary</span>
+        </div>
+        <div className="nav-item">
+          <i className="fas fa-book-open guides-icon"></i>
+          <span>Guides</span>
+        </div>
+        <div className="nav-item">
+          <i className="fas fa-database backups-icon"></i>
+          <span>Backups</span>
+        </div>
+        <div className="nav-item">
+          <i className="fas fa-globe ecosystem-icon"></i>
+          <span>Ecosystem</span>
+        </div>
+      </div>
+      <div className="social-icons">
+        <i className="fab fa-facebook-f facebook-icon"></i>
+        <i className="fab fa-twitter twitter-icon"></i>
+        <i className="fab fa-linkedin-in linkedin-icon"></i>
+      </div>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
--- client/src/layouts/home/HomePage.jsx
+++ client/src/layouts/home/HomePage.jsx
@@ -1,11 +1,12 @@
 import './HomePage.css';
-import Header from '../header/Header';
+// import Header from '../header/Header';
 import Home from './Home';
 import ServicesPage from './ServicesPage';
 import PortfolioPage from './PortfolioPage';
 import TestimonialsPage from './TestimonialsPage';
 import Footer from './Footer';
 import BusinessCTA from './BusinessCTA';
+import Header from '../../client/src/components/Header/Header';
 
 const HomePage = () => {
   
