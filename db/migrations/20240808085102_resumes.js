d84de44a67a46e366201bc2c1a2d2605bcdef641
tobias-barakaa <ngairato@gmail.com>
2024-08-08 11:54:36 +0300

    rewrite resume table

--- db/migrations/20240808085102_resumes.js
+++ db/migrations/20240808085102_resumes.js
@@ -1,15 +1,27 @@
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('resumes', table => {
+        table.string('id').primary().defaultTo(knex.raw('gen_random_uuid()'));
+        table.string('user_id').references('id').inTable('users').onDelete('CASCADE');
+        table.string('full_name').notNullable();
+        table.string('job_title').notNullable();
+        table.string('email').notNullable();
+        table.string('phone').notNullable();
+        table.text('summary');
+        table.text('skills');
+        table.text('languages');
+        table.text('certifications');
+        table.text('achievements');
+        table.integer('timeframe_id').unsigned().notNullable().references('id').inTable('timeframe');
+        table.string('user_id').notNullable().references('id').inTable('users');
+        table.decimal('cost', 10, 2).notNullable().defaultTo(0.00);
+        table.integer('status_id').unsigned().notNullable().references('id').inTable('status').defaultTo(
+            knex.raw('(SELECT id FROM status WHERE name = ?)', ['Pending'])
+          );
+        table.timestamps(true, true);
+    });
 };
 
-/**
- * @param { import("knex").Knex } knex
- * @returns { Promise<void> }
- */
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('resumes');
 };
+
