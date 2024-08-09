08d26b409e91055ac98b76d464db6a8b941d7e71
tobias-barakaa <ngairato@gmail.com>
2024-08-09 15:08:17 +0300

    article creation table

--- routes/whatt/yesyoucan/20240808084500_articlecreation.js
+++ routes/whatt/yesyoucan/20240808084500_articlecreation.js
@@ -3,17 +3,16 @@
                 table.increments('id').primary();
                 table.string('title').notNullable();
                 table.text('description').notNullable();
-                table.integer('category_id').unsigned().notNullable().references('id').inTable('categories');
+                table.string('category').defaultTo('General'); 
                 table.text('Keywords').defaultTo('').nullable();
-                table.string('complexity').notNullable();
-                table.integer('number_of_words_id').unsigned().notNullable().references('id').inTable('numberofwords');
-                table.integer('timeframe_id').unsigned().notNullable().references('id').inTable('timeframe');
-                table.integer('quantity_id').unsigned().notNullable().references('id').inTable('quantity');
-                table.string('language').defaultTo('American English');
-                  
+                table.string('complexity').notNullable().defaultTo('Basic');
+                table.integer('word_count').unsigned().notNullable().defaultTo(300);
+                table.string('duration').defaultTo('1 day');
+                table.integer('quantity').unsigned().notNullable().defaultTo(1);
+                table.string('language').defaultTo('American English');                  
                 table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE');
                 table.decimal('cost', 10, 2).notNullable().defaultTo(0.00);
-                table.integer('status_id').unsigned().notNullable().references('id').inTable('status');
+                table.enu('status', ['Pending', 'Published', 'Completed', 'Processing', 'Deleted', 'Rejected']).defaultTo('Pending');
                 table.timestamps(true, true);
             });
         };
@@ -22,10 +21,3 @@
             return knex.schema.dropTable('articlecreation');
         };
         
-
-table.integer('word_count').unsigned().notNullable().defaultTo(300);
-table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE');
-table.string('language').defaultTo('American English');
-table.enu('status', ['Pending', 'Published', 'Completed', 'Processing', 'Deleted', 'Rejected']).defaultTo('Pending');
-table.string('duration').defaultTo('1 day');
-table.string('category').defaultTo('General'); 
