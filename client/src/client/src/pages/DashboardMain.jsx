18d507b31c72f45a14842a5572192d5864b80715
tobias-barakaa <ngairato@gmail.com>
2024-08-09 20:42:31 +0300

    refactoring code

---
+++ client/src/client/src/pages/DashboardMain.jsx
@@ -0,0 +1,13 @@
+import './DashboardMain.css';
+import Home from './Home';
+
+
+const DashboardMain = () => {
+  return (
+    <div className="dashboard-main">
+      <Home />
+    </div>
+  );
+};
+
+export default DashboardMain;
--- client/src/pages/About.jsx
+++
@@ -1,9 +0,0 @@
-const About = () => {
-  return (
-    <div>
-      About Page
-    </div>
-  )
-}
-
-export default About
