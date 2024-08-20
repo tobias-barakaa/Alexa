3a7b18c7109a48ab06ae1b219194244849471a61
tobias-barakaa <ngairato@gmail.com>
2024-08-20 09:55:26 +0300

    google authentication

--- client/src/client/src/pages/ClientRegister.jsx
+++ client/src/client/src/pages/ClientRegister.jsx
@@ -1,4 +1,3 @@
-import PropTypes from 'prop-types';
 import '../styles/pages/ClientRegister.css';
 import { useEffect, useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
@@ -53,64 +52,63 @@ const ClientRegister = () => {
   return (
     <>
 
-          <form className="containerr" onSubmit={submitHandler}>
+          <form onSubmit={submitHandler}>
             
-            <p>Already have an account? 
-              <Link to={redirect ? `/login?redirect=${redirect}` : '/register'} className="link">Sign In</Link></p>
             
-            <div className="input-container">
-             
-            </div>
-            <input
-              type="text"
-              placeholder="Username"
-              className="full-width-input"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-            />
-            <input
-              type="email"
-              placeholder="Email Address"
-              className="full-width-input"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <input
-              type="password"
-              placeholder="Password"
-              className="full-width-input"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <button type="submit"
+            
+            
+
+
+
+              <div className="signup-box">
+<div className="signup-title-container">
+    <h3 className="signup-title">Sign Up</h3>
+  </div>
+  <input type="text" className="signup-input"
+  placeholder="Username"
+  value={username}
+  onChange={(e) => setUsername(e.target.value)}
+  />
+  <input 
+  className="signup-input"
+  type="email"
+  placeholder="Email Address"
+  value={email}
+  onChange={(e) => setEmail(e.target.value)}
+  />
+  <input className="signup-input"
+  type="password"
+  placeholder="Password"
+  value={password}
+  onChange={(e) => setPassword(e.target.value)}
+  
+  />
+  <button className="signup-button">Sign Up</button>
+  <button className="google-button-signup">Sign up with Google</button>
+  <button type="submit"
              className="signup-btn"
              disabled={isLoading}
              >Sign Up as Client</button>
+ <button className="google-button">
+        <i className="fab fa-google"></i>
+        Google
+      </button> 
+  <a className="login-link">Already have an account? <Link to={redirect ? `/login?redirect=${redirect}` : '/register'} className="link">Sign In</Link>
+
+
+  </a>
+              
+</div>
+
+
+            
+            
           </form>
     </>
   );
 };
 
-ClientRegister.propTypes = {
-  handleCloseModal: PropTypes.func.isRequired,
-};
 
 export default ClientRegister;
 
 
-
-// <div className="signup-box">
-// <div className="signup-title-container">
-//     <h3 className="signup-title">Sign Up</h3>
-//   </div>
-//   <input type="text" placeholder="Name" className="signup-input" />
-//   <input type="email" placeholder="Email" className="signup-input" />
-//   <input type="password" placeholder="Password" className="signup-input" />
-//   <button className="signup-button">Sign Up</button>
-//   <button className="google-button-signup">Sign up with Google</button>
-{/* <button className="google-button">
-        <i className="fab fa-google"></i>
-        Google
-      </button> */}
-//   <a href="#" className="login-link">Already have an account? Log in</a>
-// </div>
