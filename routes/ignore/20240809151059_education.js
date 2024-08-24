d0c860e50c4d5fe53ec535421b509e2c4afefe6b
tobias-barakaa <ngairato@gmail.com>
2024-08-24 16:59:08 +0300

    configuring superpase

---
+++ routes/ignore/20240809151059_education.js
@@ -0,0 +1,16 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('education', table => {
+        table.increments('id').primary();
+        table.integer('resume_id').references('id').inTable('resumes').onDelete('CASCADE');
+        table.string('degree').notNullable();
+        table.string('institution').notNullable();
+        table.date('start_date').notNullable();
+        table.date('end_date').notNullable();
+        table.text('description');
+        table.timestamps(true, true);
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('education');
+};
