8875aba27179d803ba17657c454208df304769db
tobias-barakaa <ngairato@gmail.com>
2024-09-05 15:14:42 +0300

    making progress

--- frontend/src/client/src/pages/PasswordReset.jsx
+++ frontend/src/client/src/pages/PasswordReset.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import './PasswordReset.css'; 
+import { Toast } from 'bootstrap';
 
 const PasswordReset = () => {
     const [email, setEmail] = useState('');
+    const  [message, setMessage] = useState('');
     const  setVal = (e) => {
         e.preventDefault();
         setEmail(e.target.value);
@@ -10,7 +12,26 @@ const PasswordReset = () => {
 
     const sendLink = async(e) => {
         e.preventDefault();
-        console.log(email);
+        const res = await fetch('http://localhost:5000/api/users/sendpasswordlink', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({email})
+        });
+
+        const data = await res.json();
+
+        if(data.status == 201) {
+            setEmail("");
+            setMessage(true)
+        } else {
+            Toast.show("Error in sending password reset link", {
+                delay: 2000,
+                autohide: true
+            });
+        }
+        
     }
 
     
@@ -18,6 +39,11 @@ const PasswordReset = () => {
     <div className="reset-password-container">
       <div className="reset-password-form">
         <p className="reset-password-heading">Enter your email</p>
+        
+        {
+            message ? <p style={{ color: "green", fontWeight: "bold" }}>Password reset link sent succesfull in Your Email</p>
+            : null
+        }
         <form>
           <input
             type="email"
