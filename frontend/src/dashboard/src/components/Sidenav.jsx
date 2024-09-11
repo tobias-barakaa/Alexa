8d013dfbf078a040b984d47a9ef0e58ae809d854
tobias-barakaa <ngairato@gmail.com>
2024-09-11 16:39:00 +0300

    commited to one item

--- frontend/src/dashboard/src/components/Sidenav.jsx
+++ frontend/src/dashboard/src/components/Sidenav.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link, useLocation } from "react-router-dom";
-import logo from "../../../client/src/assets/images/logo.png";
+// import logo from "../../../client/src/assets/images/logo.png";
 import "./Sidenav.css";
 import SidebarProfile from "./SidebarProfile";
 
@@ -30,7 +30,12 @@ const Sidebar = () => {
           className={`sidebar-menu-link ${activeLink === '/dashboard' || activeLink === '/dashboard/' ? 'active' : ''}`}
           onClick={() => handleLinkClick('/dashboard')}
         >
-          <img src={logo} alt="Logo" className="logo" />
+          
+          <div className="logo-container">
+          <span className="logo-text">en<span className='dot'>.</span>writers</span>
+        </div>
+
+
         </Link>
             </div>
             </div>
@@ -143,100 +148,7 @@ const Sidebar = () => {
       </div>
     </>
 
-    // <div className="sidena">
-    //   <div className="sidebar-menu-container">
-    //     <Link
-    //       to="/dashboard"
-    //       className={`sidebar-menu-link ${activeLink === '/dashboard' || activeLink === '/dashboard/' ? 'active' : ''}`}
-    //       onClick={() => handleLinkClick('/dashboard')}
-    //     >
-    //       <img src={logo} alt="Logo" className="logo" />
-    //     </Link>
-    //     <hr className="dark-divider" />
-    //     <div className="sidebar-menu-content">
-    //       <ul className="sidebar-menu-list">
-    //         <li className="sidebar-menu-item">
-    //           <Link
-    //             to="/dashboard/blogorder"
-    //             className={`sidebar-menu-link ${activeLink === '/dashboard/blogorder' ? 'active' : ''}`}
-    //             onClick={() => handleLinkClick('/dashboard/blogorder')}
-    //           >
-    //             <i className="bi bi-pencil-square sidebar-menu-icon"></i>
-    //             <span className="sidebar-menu-text">Blog Writing</span>
-    //           </Link>
-    //         </li>
-    //         <li className="sidebar-menu-item">
-    //           <Link
-    //             to="/dashboard/articlecreation"
-    //             className={`sidebar-menu-link ${activeLink === '/dashboard/articlecreation' ? 'active' : ''}`}
-    //             onClick={() => handleLinkClick('/dashboard/articlecreation')}
-    //           >
-    //             <i className="bi bi-bookshelf sidebar-menu-icon"></i>
-    //             <span className="sidebar-menu-text">Article Creation</span>
-    //           </Link>
-    //         </li>
-    //         <li className="sidebar-menu-item">
-    //           <Link
-    //             to="/dashboard/resumecvwriting"
-    //             className={`sidebar-menu-link ${activeLink === '/dashboard/resumecvwriting' ? 'active' : ''}`}
-    //             onClick={() => handleLinkClick('/dashboard/resumecvwriting')}
-    //           >
-    //             <i className="bi bi-card-list sidebar-menu-icon"></i>
-    //             <span className="sidebar-menu-text">Resume/CV Writing</span>
-    //           </Link>
-    //         </li>
-    //         <li className="sidebar-menu-item">
-    //           <Link
-    //             to="/dashboard/emailcopywriting"
-    //             className={`sidebar-menu-link ${activeLink === '/dashboard/emailcopywriting' ? 'active' : ''}`}
-    //             onClick={() => handleLinkClick('/dashboard/emailcopywriting')}
-    //           >
-    //             <i className="bi bi-printer sidebar-menu-icon"></i>
-    //             <span className="sidebar-menu-text">Email Copywriting</span>
-    //           </Link>
-    //         </li>
-    //       </ul>
-    //     </div>
-    //   </div>
-    //   <div className="manage-container">
-    //     <div className="sidebar-menu-container">
-    //       <hr className="dark-divider" />
-    //       <ul className="manage-list">
-    //         <li className="sidebar-menu-item">
-    //           <Link
-    //             to="/dashboard/editorders"
-    //             className={`sidebar-menu-link ${activeLink === '/dashboard/editorders' ? 'active' : ''}`}
-    //             onClick={() => handleLinkClick('/dashboard/editorders')}
-    //           >
-    //             <i className="bi bi-pencil-square sidebar-menu-icon"></i>
-    //             <span className="sidebar-menu-text">Edit Orders</span>
-    //           </Link>
-    //         </li>
-    //         <li className="sidebar-menu-item">
-    //           <Link
-    //             to="/dashboard/ordershistory"
-    //             className={`sidebar-menu-link ${activeLink === '/dashboard/ordershistory' ? 'active' : ''}`}
-    //             onClick={() => handleLinkClick('/dashboard/ordershistory')}
-    //           >
-    //             <i className="bi bi-hourglass sidebar-menu-icon"></i>
-    //             <span className="sidebar-menu-text">Order History</span>
-    //           </Link>
-    //         </li>
-    //         <li className="sidebar-menu-item">
-    //           <Link
-    //             to="/dashboard/completedorders"
-    //             className={`sidebar-menu-link ${activeLink === '/dashboard/completedorders' ? 'active' : ''}`}
-    //             onClick={() => handleLinkClick('/dashboard/completedorders')}
-    //           >
-    //             <i className="bi bi-building-check sidebar-menu-icon"></i>
-    //             <span className="sidebar-menu-text">Completed Orders</span>
-    //           </Link>
-    //         </li>
-    //       </ul>
-    //     </div>
-    //     <SidebarProfile />
-    //   </div>
-    // </div>
+    
   );
 };
 
