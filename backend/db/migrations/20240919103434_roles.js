a89fa575835ec22322ad0d7204dc5729f75afbd8
tobias-barakaa <ngairato@gmail.com>
2024-09-19 13:37:23 +0300

    make users and roles table

---
+++ backend/db/migrations/20240919103434_roles.js
@@ -0,0 +1,12 @@
+// YYYYMMDDHHMMSS_create_roles_table.js
+exports.up = function(knex) {
+    return knex.schema.createTable('roles', function(table) {
+      table.increments('id').primary();
+      table.string('name').notNullable().unique();
+      table.timestamps(true, true);
+    });
+  };
+  
+  exports.down = function(knex) {
+    return knex.schema.dropTable('roles');
+  };
