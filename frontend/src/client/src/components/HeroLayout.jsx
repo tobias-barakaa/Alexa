0bf7bf69b29825636eb5b879d6dda7ec383834d1
tobias-barakaa <ngairato@gmail.com>
2024-10-03 14:47:24 +0300

    grid layout starting to look good

--- frontend/src/client/src/components/HeroLayout.jsx
+++ frontend/src/client/src/components/HeroLayout.jsx
@@ -1,25 +1,28 @@
 import "./HeroLayout.css";
-import google from '../assets/images/googleimg.png';
-import free from '../assets/images/free.jpg';
-import alexa from '../assets/images/alexa.jpg';
-import writer from '../assets/images/writer.jpg';
 
 const HeroLayout = () => {
   return (
     <div className="wrapper">
-      <div className="left-side">Left Side</div>
-      <div className="right-sidee">
-        <div className="item1">item 1</div>
-        <div className="item2">item 2</div>
-        <div className="item3">item 3</div>
-        <div className="item4">item 4</div>
-        <div className="item5">item 5</div>
-        
-
-
-
-
+      <div className="left-sidee">
+        <h1>Left Side Content</h1>
+      </div>
       
+      <div className="right-sidee">
+        <div className="item1 image-home">
+          <img />
+        </div>
+        <div className="item2 image-home">
+          <img />
+        </div>
+        <div className="item3 image-home">
+          <img  />
+        </div>
+        <div className="item4 image-home">
+          <img />
+        </div>
+        <div className="item5 image-home">
+          <img />
+        </div>
       </div>
     </div>
   );
